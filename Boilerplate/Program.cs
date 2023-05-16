4fcc10ee65e44a389893854ae4eceb62bc8dbc82
druss-dev <druss.dev@gmail.com>
2023-05-16 00:05:54 -0400

    added easier way to call methods

--- Boilerplate/Program.cs
+++ Boilerplate/Program.cs
@@ -2,21 +2,21 @@
 using Boilerplate.Services.Thing;
 using Boilerplate.Utilities;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddConfigurationOptions();
 builder.AddLogging();
 builder.AddInternalServices();
 
-//var application = builder.Build();
-
-// gets instance of a service
-var thingServiceType = Type.GetType(ServiceType.ThingService);
-var thingService = (ThingService)Activator.CreateInstance(thingServiceType!)!;
-Console.WriteLine(thingService.GetTheThing().Result.StringyThing);
+var application = builder.Build();
 
 // use this when accessing encrypted columns
 //await application.AddEncryptionProvider();
 
-// need this is the console needs to keep running
-//await application.RunAsync();
+await application.StartAsync();
+
+var thingService = application.Services.GetRequiredService<IThingService>();
+
+var thing = thingService.GetTheThing();
+Console.WriteLine(thing.Result.StringyThing);
